2d64c3574a7ebd8c1429ee8c8ecf0eec46331ce5
Hung Quach <hung.quach@gmail.com>
2020-11-24 00:23:10 -0800

    fdsafas

--- electron/main.js
+++ electron/main.js
@@ -1,62 +1,57 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const url = require('url');
-const sqlite3 = require('sqlite3');
-const { channels } = require('../src/shared/constants');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+const sqlite3 = require("sqlite3");
+const { channels } = require("../src/shared/constants");
 
 // Set up Sqlite3
-const userData = app.getPath('userData');
-const dbFile = path.resolve(userData, 'db.sqlite3');
+const userData = app.getPath("userData");
+const dbFile = path.resolve(userData, "db.sqlite3");
 const db = new sqlite3.Database(dbFile, (err) => {
-  if (err) console.error('Database opening error', err) 
-  console.log(`sqlite debug:`, {err, dbFile, userData})
-})
-
+    if (err) console.error("Database opening error", err);
+    console.log(`sqlite debug:`, { err, dbFile, userData });
+});
 
 let mainWindow;
 
-function createWindow () {
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../index.html'),
-    protocol: 'file:',
-    slashes: true,
-  });
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  mainWindow.loadURL(startUrl);
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+function createWindow() {
+    const startUrl =
+        process.env.ELECTRON_START_URL ||
+        url.format({
+            pathname: path.join(__dirname, "../index.html"),
+            protocol: "file:",
+            slashes: true,
+        });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    mainWindow.loadURL(startUrl);
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on("activate", function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 ipcMain.on(channels.APP_INFO, (event) => {
-  console.log({db})
-  const sql = `SELECT * FROM memberships`
-  db.all(sql, (err, rows) => {
-    if(err) console.log({err})
-    event.sender.send(channels.APP_INFO, { 
-      appName: app.getName(),
-      appVersion: app.getVersion(),
-      rows: rows
+    event.sender.send(channels.APP_INFO, {
+        appName: app.getName(),
+        appVersion: app.getVersion(),
     });
-  })
 });
