bd3e4acac14a67ab453216df6bc18e4a7cd69c64
Hung Quach <hung.quach@gmail.com>
2021-01-23 17:55:10 -0800

    remove phone

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Find from './components/Find/Find';
-import Member from './components/Portal/MembershipPortal';
+import MemberScreen from './components/Member/MemberScreen';
 import BuyScreen from './components/Buy/BuyScreen';
 import AddScreen from './components/Add/AddScreen';
 import './App.css';
@@ -14,7 +14,7 @@ const App = () => {
             <Switch>
                 <Route exact path='/' component={Login} />
                 <Route exact path='/find' component={Find} />
-                <Route exact path='/member' component={Member} />
+                <Route exact path='/member' component={MemberScreen} />
                 <Route exact path='/account' component={BuyScreen} />
                 <Route exact path='/add' component={AddScreen} />
             </Switch>
